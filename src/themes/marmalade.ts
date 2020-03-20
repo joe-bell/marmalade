a36164f38a617a0864c5016cdd8c649e25cd7e66
joe-bell <joe@joebell.co.uk>
2020-03-20 17:23:40 +0200

    feat: steal B&Q colour scheme

--- src/themes/marmalade.ts
+++ src/themes/marmalade.ts
@@ -8,7 +8,7 @@ export default {
   colors: {
     text: "hsl(10, 20%, 20%)",
     background: "hsl(10, 10%, 98%)",
-    primary: "hsl(10, 80%, 50%)",
+    primary: "#f47d1e",
     secondary: "hsl(10, 60%, 50%)",
     highlight: "hsl(10, 40%, 90%)",
     purple: "hsl(250, 60%, 30%)",
